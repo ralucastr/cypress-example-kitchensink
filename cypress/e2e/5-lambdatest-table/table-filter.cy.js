d3c4136cc768ac0117a7e8527ca0155a7cd290db
ralucastr <129978480+ralucastr@users.noreply.github.com>
2023-05-25 20:16:36 +0300

    asdf

--- cypress/e2e/5-lambdatest-table/table-filter.cy.js
+++ cypress/e2e/5-lambdatest-table/table-filter.cy.js
@@ -1,14 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Table Filters', () => {
-    beforeEach(() => {
+  beforeEach(() => {
     cy.setCookie('exit_popup_dismissed', 'closed');
 
     Cypress.on('uncaught:exception', () => {
-        // Log the error or perform any custom handling
-        // Returning false prevents Cypress from failing the test
-        return false;
-      });
+      // Log the error or perform any custom handling
+      // Returning false prevents Cypress from failing the test
+      return false;
+    });
 
     cy.visit(
       'https://www.lambdatest.com/selenium-playground/table-records-filter-demo'
@@ -17,41 +17,41 @@ describe('Table Filters', () => {
 
   it('Filtering By Any Option Returns The Correct Records', () => {
     const tests = [
-        {
-          filter: 'button[data-target="all"]',
-          result: 5 
-        },
-        {
-          filter: 'button[data-target="pagado"]',
-          result: 2,
-          label: 'span.pull-right.pagado',
-          text: '(Green)'
-        },
-        {
-          filter: 'button[data-target="pendiente"]',
-          result: 2,
-          label: 'span.pull-right.pendiente',
-          text: '(Orange)'
-        },
-        {
-          filter: 'button[data-target="cancelado"]',
-          result: 1,
-          label: 'span.pull-right.cancelado',
-          text: '(Red)'
-        }
-      ]
+      {
+        filter: 'button[data-target="all"]',
+        result: 5 
+      },
+      {
+        filter: 'button[data-target="pagado"]',
+        result: 2,
+        label: 'span.pull-right.pagado',
+        text: '(Green)'
+      },
+      {
+        filter: 'button[data-target="pendiente"]',
+        result: 2,
+        label: 'span.pull-right.pendiente',
+        text: '(Orange)'
+      },
+      {
+        filter: 'button[data-target="cancelado"]',
+        result: 1,
+        label: 'span.pull-right.cancelado',
+        text: '(Red)'
+      }
+    ]
 
-       tests.forEach((test) => {         
-        cy.get(test.filter).click();
-        cy.get('table.table.table-filter')
+    tests.forEach((test) => {         
+      cy.get(test.filter).click();
+      cy.get('table.table.table-filter')
             .find('tr')
             .filter(':visible')
             .should('have.length', test.result);
 
 
-        if (test.filter !== 'button[data-target="all"]') {
-            cy.get(test.label).should('contain', test.text);
-        }       
-       })
+      if (test.filter !== 'button[data-target="all"]') {
+        cy.get(test.label).should('contain', test.text);
+      }       
+    })
   })
 })
